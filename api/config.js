0ea62eb7e6ae95aa1bb25341e8774eeb1830b0cf
valentinmadiot <valentin.madiot@gmail.com>
2025-02-26 02:28:31 +0700

    upgrade

---
+++ api/config.js
@@ -0,0 +1,34 @@
+//! GESTION DU PORT :
+//* "normalizePort" => RENVOIE UN PORT VALIDE, QU'IL SOIT FOURNI SOUR LA FORME D'UN NUMERO OU D'UNE CHAINE
+normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+exports.port = normalizePort(process.env.PORT || "3000");
+
+//* "errorHandler" => RECHERCHE LES DIFFERENTES ERREURS ET LES GERES DE MANIERE APPROPRIEE
+exports.errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+    default:
+      throw error;
+  }
+};
