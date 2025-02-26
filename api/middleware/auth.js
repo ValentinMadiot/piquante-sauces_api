f6d74f035d88e3119330504f6c4f53ac21867b84
valentinmadiot <valentin.madiot@gmail.com>
2025-02-27 02:28:09 +0700

    update

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,35 +1,22 @@
+//* IMPORT JSONWEBTOKEN
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
   try {
-    console.log("Authorization Header:", req.headers.authorization);
+    //* RECUPERER HEADER "Authorization" ET GARDER SEULEMENT LE TOKEN GRACE A LA METHODE ".split"
     const token = req.headers.authorization.split(" ")[1];
-    console.log("Extracted Token:", token);
+    //* DECODER LE TOKEN AVEC LA METHODE "verify" AVEC EN ARGUMENTS (le token, clé secrète enrengistré dans .env)
     const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
-    console.log("Decoded Token:", decodedToken);
-    req.auth = { userId: decodedToken.userId };
+    //* RECUPERER "userId" DU TOKEN
+    const userId = decodedToken.userId;
+    //* AJOUTER "userId" POUR QUE NOS ROUTES PUISSENT L'UTILISER
+    req.auth = {
+      userId: userId,
+    };
     next();
   } catch (error) {
-    console.error("Auth Error:", error);
     res.status(401).json({ error });
   }
+  console.log("JWT_TOKEN:", process.env.JWT_TOKEN);
 };
-
-// //* IMPORT JSONWEBTOKEN
-// const jwt = require('jsonwebtoken')
-
-// module.exports = (req, res, next) => {
-//   try {
-//     //* RECUPERER HEADER "Authorization" ET GARDER SEULEMENT LE TOKEN GRACE A LA METHODE ".split"
-//     const token = req.headers.authorization.split(' ')[1]
-//     //* DECODER LE TOKEN AVEC LA METHODE "verify" AVEC EN ARGUMENTS (le token, clé secrète enrengistré dans .env)
-//     const decodedToken = jwt.verify(token, process.env.JWT_TOKEN)
-//     //* RECUPERER "userId" DU TOKEN
-//     const userId = decodedToken.userId
-//     //* AJOUTER "userId" POUR QUE NOS ROUTES PUISSENT L'UTILISER
-//     req.auth = {
-//       userId: userId
-//     }
-//     next()
-//   }
-//   catch(error) {res.status(401).json({ error })}
-// }
-// //* ENSUITE => AJOUTER "auth" AUX ROUTES Sauce AVANT CHAQUE CONTROLLER
+//* ENSUITE => AJOUTER "auth" AUX ROUTES Sauce AVANT CHAQUE CONTROLLER
